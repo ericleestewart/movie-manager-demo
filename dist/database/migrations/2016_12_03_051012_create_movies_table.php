17eb4a6f8895cda6a014dddb0f3bf2febf10f272
Eric Stewart <estewart@sans.org>
2016-12-03 00:20:31 -0500

    Create movies table.

---
+++ dist/database/migrations/2016_12_03_051012_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title', 50);
+            $table->enum('format', ['Blu-ray', 'DVD', 'Streaming', 'VHS']);
+            $table->integer('length');
+            $table->integer('year');
+            $table->enum('rating', ['1', '2', '3', '4', '5'])->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
