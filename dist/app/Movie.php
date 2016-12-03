cabbb60bf834d13399a82c5dee345b80435e8a90
Eric Stewart <estewart@sans.org>
2016-12-03 00:53:25 -0500

    Create the base movie model.

---
+++ dist/app/Movie.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'format', 'length', 'year', 'rating'];
+}
